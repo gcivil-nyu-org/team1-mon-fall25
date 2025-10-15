1acf4b05fe578b8eeadb10a3288bef4369d5e077
Thaddeus <thaddeuspearson@gmail.com>
2025-10-15 23:19:40 +0200

    ud static var order

--- simpletix/config/settings.py
+++ simpletix/config/settings.py
@@ -153,8 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = "/var/www/simpletix/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Default primary key field type
