8ed41107861fac9339929261e26cbd9b7f9d64d4
lakshmihr15 <1rn21ai159.lakshmihr@rnsit.ac.in>
2025-10-18 16:33:18 -0400

    Feature 41: Full CRUD for Events and Tickets with templates and formsets

--- simpletix/config/settings.py
+++ simpletix/config/settings.py
@@ -46,16 +46,22 @@ else:
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": secrets["dbname"],
-        "USER": secrets["username"],
-        "PASSWORD": secrets["password"],
-        "HOST": secrets["host"],
-        "PORT": secrets["port"],
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
