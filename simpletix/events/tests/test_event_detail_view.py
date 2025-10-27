6f175517a5c9d497be4c63b16c353f49621b6923
Cesar Sanchez <cesarsanchez@Cesars-MacBook-Pro.local>
2025-10-27 03:12:42 -0400

    test: add event detail 404 test to raise coverage above 85%

---
+++ simpletix/events/tests/test_event_detail_view.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class EventDetailViewTest(TestCase):
+    def test_event_detail_view_returns_404_for_nonexistent_event(self):
+        # Trying to access a non-existent event should return 404
+        response = self.client.get(reverse("events:event_detail", args=[999]))
+        self.assertEqual(response.status_code, 404)
