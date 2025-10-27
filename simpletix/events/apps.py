236f3b07882f74d81760fb123126ecf2ebd0d2c8
Cesar Sanchez <cesarsanchez@Cesars-MacBook-Pro.local>
2025-10-27 03:07:50 -0400

    style: fix black and flake8 formatting issues

--- simpletix/events/apps.py
+++ simpletix/events/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 import os
 
+
 class EventsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "events"
--- simpletix/events/tests/test_dummy_event_list.py
+++ simpletix/events/tests/test_dummy_event_list.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 class EventListViewTest(TestCase):
     def test_event_list_view_loads(self):
         response = self.client.get(reverse("events:event_list"))
