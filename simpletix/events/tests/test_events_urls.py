b8964864006dc5119f991802e7c3489af6147279
lakshmihr15 <1rn21ai159.lakshmihr@rnsit.ac.in>
2025-10-28 21:59:48 -0400

    Add updated tests for Event views

--- simpletix/events/tests/test_events_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import reverse, resolve
-from events import views
-
-
-def test_event_list_url_resolves():
-    url = reverse("events:event_list")
-    assert resolve(url).func == views.event_list
