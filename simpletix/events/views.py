50d85286743b71bb3d178ef176db5dabbdddd678
lakshmihr15 <1rn21ai159.lakshmihr@rnsit.ac.in>
2025-11-13 16:07:10 -0500

    Fix event list display, hide lat/long fields, and pass flake8 tests

--- simpletix/events/views.py
+++ simpletix/events/views.py
@@ -256,9 +256,8 @@ def delete_event(request, event_id):
 
 
 def event_list(request):
-    events = Event.objects.all().prefetch_related("ticketInfo").order_by("-date")
+    events = Event.objects.all().prefetch_related("ticketInfo", "time_slots").order_by("-id")
     return render(request, "events/event_list.html", {"events": events})
-
 
 def event_detail(request, event_id):
     event = get_object_or_404(Event.objects.prefetch_related("time_slots"), id=event_id)
